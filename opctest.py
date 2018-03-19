36ffc04f919262f392ded4bebc19f2a88e314897
adilj13 <adil.j13@gmail.com>
2018-03-19 22:12:23 +0500

    Updated List and code accordingly.

--- opctest.py
+++ opctest.py
@@ -7,7 +7,7 @@ from urllib.parse import urlparse, parse_qs
 opc = OpenOPC.client()
 opc.connect('Kepware.KEPServerEnterprise.V5')
 PORT_NUMBER = 8080
-tags=['Inlet_Nitrogen','Inlet_Methane','Inlet_CO2','Inlet_Ethane','Inlet_C5','Inlet_Propane','Inlet_IButane','Inlet_NButane','Inlet_Vapour_Pressure','Inlet_BTU','Inlet_Gas_SG','Inlet_Compressibility','Inlet_Gallons_MSCF','Inlet_Molecular_Weight','Residue_Nitrogen','Residue_Methane','Residue_CO2','Residue_Ethane','Residue_C5','Residue_Propane','Residue_IButane','Residue_NButane','Residue_Vapour_Pressure','Residue_BTU','Residue_Gas_SG','Residue_Compressibility','Residue_Gallons_MSCF','Residue_Molecular_Weight']
+tags=["Inlet_BTU","Inlet_C5","Inlet_CO2","Inlet_Compressibility","Inlet_Ethane","Inlet_Gallons_MSCF","Inlet_Gas_SG","Inlet_IButane","Inlet_Methane","Inlet_Molecular_Weight","Inlet_NButane","Inlet_Nitrogen","Inlet_Propane","Inlet_Vapour_Pressure","Residue_BTU","Residue_C5","Residue_CO2","Residue_Compressibility","Residue_Ethane","Residue_Gallons_MSCF","Residue_Gas_SG","Residue_IButane","Residue_Methane","Residue_Molecular_Weight","Residue_NButane","Residue_Nitrogen","Residue_Propane","Residue_Vapour_Pressure","Tag1","Tag2",]
 target_tags=[]
 #This class will handles any incoming request from
 #the browser 
@@ -21,13 +21,13 @@ class myHandler(BaseHTTPRequestHandler):
 		#GET query content
 		query_components = parse_qs(urlparse(self.path).query)
 		
-		if(query_components["data"]):
-			data = query_components["data"]
-			self.wfile.write(json.dumps({"name":opc.read(data), "port":"7766", "data":"adad"}).encode())
-		else:
-			for tag in tags:
-				target_tags.append(opc.read(tag))
-			self.wfile.write(json.dumps({"name":target_tags, "port":"7766", "data":"adad"}).encode())
+		#if(query_components["data"]):
+		#	data = query_components["data"]
+		#	self.wfile.write(json.dumps({"name":opc.read(data), "port":"7766", "data":"adad"}).encode())
+		#else:
+		for tag in tags:
+			target_tags.append(opc.read("Channel1.Device1."+tag))
+		self.wfile.write(json.dumps({"name":target_tags, "port":"7766", "data":"adad"}).encode())
 		# Send the html message
 		return
 
@@ -43,4 +43,3 @@ try:
 except KeyboardInterrupt:
 	print ('^C received, shutting down the web server')
 	server.socket.close()
-	
